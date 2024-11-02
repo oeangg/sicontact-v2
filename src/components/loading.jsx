5f34c8aff92ec4c5e94d61fc59511c980faa562b
Your Nameoeangg <msubhan33@gmail.com>
2024-11-02 21:55:25 +0700

    react suspense 3

--- src/components/loading.jsx
+++ src/components/loading.jsx
@@ -1,6 +1,6 @@
 export const Loading = () => {
   return (
-    <div className=" h-screen h-screen flex animate-pulse  ">
+    <div className=" h-screen  flex animate-pulse  ">
       <div className="max-w-4xl w-full mx-auto bg-background  rounded-xl   flex flex-col gap-1  items-center h-4/5 ">
         <h1 className="m-auto text-2xl font-semibold text-teal-600 ">
           Loading data...
