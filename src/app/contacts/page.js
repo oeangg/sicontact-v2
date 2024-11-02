63d39356d78243433e669b76ac8fdec065e08f8a
Your Nameoeangg <msubhan33@gmail.com>
2024-11-02 21:52:02 +0700

    react suspense 2

--- src/app/contacts/page.js
+++ src/app/contacts/page.js
@@ -16,18 +16,18 @@ export default async function ContactPage({ searchParams }) {
 
   return (
     <div className=" w-full h-screen bg-foreground py-10 px-4 sm:px-0 shadow-xl">
-      <div className="max-w-4xl   w-full mx-auto bg-background  rounded-xl   flex flex-col gap-1  items-center ">
-        <div className="flex  gap-2 w-full px-5 pt-7 pb-4 ">
-          <Link href="/" className="w-2/5">
-            <h1 className=" text-3xl text-red-300 font-bold font-Pacifico ">
-              Si<span className="text-red-400 text-4xl">Contacts/...</span>
-            </h1>
-          </Link>
-          <SearchBox />
-          <BtnAddContact />
-        </div>
+      <React.Suspense fallback={<Loading />}>
+        <div className="max-w-4xl   w-full mx-auto bg-background  rounded-xl   flex flex-col gap-1  items-center ">
+          <div className="flex  gap-2 w-full px-5 pt-7 pb-4 ">
+            <Link href="/" className="w-2/5">
+              <h1 className=" text-3xl text-red-300 font-bold font-Pacifico ">
+                Si<span className="text-red-400 text-4xl">Contacts/...</span>
+              </h1>
+            </Link>
+            <SearchBox />
+            <BtnAddContact />
+          </div>
 
-        <React.Suspense fallback={<Loading />}>
           <GridContact query={query} currentPages={page} />
 
           <SumContacts query={query} />
@@ -35,11 +35,11 @@ export default async function ContactPage({ searchParams }) {
           <div className="w-full flex justify-center items-center mb-6">
             <Pagination totalPages={totalPages} />
           </div>
-        </React.Suspense>
-      </div>
-      <p className="pt-8 text-teal-100 font-light text-xs text-center">
-        Made with 😍 by Subhan Mohammad
-      </p>
+        </div>
+        <p className="pt-8 text-teal-100 font-light text-xs text-center">
+          Made with 😍 by Subhan Mohammad
+        </p>
+      </React.Suspense>
     </div>
   );
 }
--- src/components/loading.jsx
+++ src/components/loading.jsx
@@ -1,9 +1,11 @@
 export const Loading = () => {
   return (
-    <div className=" h-screen w-full flex ">
-      <h1 className="m-auto text-2xl font-bold text-teal-500">
-        Loading data...
-      </h1>
+    <div className=" h-screen h-screen flex animate-pulse  ">
+      <div className="max-w-4xl w-full mx-auto bg-background  rounded-xl   flex flex-col gap-1  items-center h-4/5 ">
+        <h1 className="m-auto text-2xl font-semibold text-teal-600 ">
+          Loading data...
+        </h1>
+      </div>
     </div>
   );
 };
